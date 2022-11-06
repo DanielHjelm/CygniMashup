8739d86c10f969502be2be0eb84083b598240e4b
DHjeelm <dnl1@live.se>
2022-11-06 09:39:38 +0100

    Removed redudant code

--- utils/fetch_cover_art.js
+++ utils/fetch_cover_art.js
@@ -5,13 +5,8 @@ async function fetchCoverArt(albumID){
     // Use the Cover Art Archive API with the specific album ID to fetch image
     try {
         return await axios.get(`http://coverartarchive.org/release-group/${albumID}`).then(res => {
-            if(res.status == 200) {
-                return res.data.images[0].image;
-
-            } else {
-                return "Cover art not found"
-            }
-          
+                // Return link to image
+                return res.data.images[0].image;         
         });
       } catch (error) {
         return `Error when fetching cover art. Error code: ${error.response.status}`
